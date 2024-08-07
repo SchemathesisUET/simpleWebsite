f4d18fa334faf0c9c9470b88e47027c9362beefa
Đặng Tiến Dũng <dangtiendung1201@gmail.com>
2024-08-07 10:25:42 +0000

    feat: Test the simplest successfully

--- simple-api/test_api.py
+++ simple-api/test_api.py
@@ -1,7 +1,7 @@
 import schemathesis
 import requests
 
-schema = schemathesis.from_path("openapi.yaml")
+schema = schemathesis.from_path("openapi.yaml", base_url="http://localhost:5000")
 
 @schema.parametrize()
 def test_api(case):
