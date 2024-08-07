43fee19e89d141ea02b25003346db1be082aa35d
Đặng Tiến Dũng <dangtiendung1201@gmail.com>
2024-08-07 12:20:25 +0000

    fix: Testcase

--- simple-api/app.py
+++ simple-api/app.py
@@ -2,16 +2,16 @@ from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 
-# API hello to greet the user. The user name only accepts string that consists of alphabets only. If not then it will return an error message.
+# API hello to greet the user. The user name only accepts string that consists of alphabets only and have at least 1 alphabets. If not then it will return an error message.
 @app.route("/hello", methods=["GET"])
 def hello():
     name = request.args.get("name")
-    if name.isalpha():
+    if name.isalpha() and len(name) > 0:
         return jsonify({"message": f"Hello, {name}!"})
     else:
-        return jsonify({"message": "Name should only contain alphabets."}), 400
+        return jsonify({"message": "Name should be a string that consists of alphabets only and have at least 1 alphabet."}), 400
 
-# API to add two numbers. The numbers should be integers. If not then it will return an error message.
+# API to add two numbers. The numbers should be positive integers. If not then it will return an error message.
 @app.route("/add", methods=["GET"])
 def add():
     num1 = request.args.get("num1")
