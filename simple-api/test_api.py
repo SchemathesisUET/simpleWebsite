13a411c51634558b4f22a6c23edbab61b48e71ce
Đặng Tiến Dũng <dangtiendung1201@gmail.com>
2024-08-07 11:37:09 +0000

    feat: Update online API link

--- simple-api/test_api.py
+++ simple-api/test_api.py
@@ -7,7 +7,7 @@ from schemathesis.cli import run
 load_dotenv()
 
 # Use the base URL of your running Flask app
-schema = schemathesis.from_path("openapi.yaml", base_url="https://improved-waddle-4vjj97p6wj5f77jr-5000.app.github.dev")
+schema = schemathesis.from_path("openapi.yaml", base_url="https://simplewebsite-y70r.onrender.com")
 
 @schema.parametrize()
 def test_api(case):
