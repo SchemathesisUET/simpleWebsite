e2c6a01146b088e68340adc3982c1d4f47a31e5f
Đặng Tiến Dũng <dangtiendung1201@gmail.com>
2024-08-07 11:54:55 +0000

    feat: API to add number

--- simple-api/app.py
+++ simple-api/app.py
@@ -2,10 +2,24 @@ from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 
+# API hello to greet the user. The user name only accepts string that consists of alphabets only. If not then it will return an error message.
 @app.route("/hello", methods=["GET"])
 def hello():
-    name = request.args.get("name", "World")
-    return jsonify({"message": f"Hello, {name}!"})
+    name = request.args.get("name")
+    if name.isalpha():
+        return jsonify({"message": f"Hello, {name}!"})
+    else:
+        return jsonify({"message": "Name should only contain alphabets."}), 400
+
+# API to add two numbers. The numbers should be integers. If not then it will return an error message.
+@app.route("/add", methods=["GET"])
+def add():
+    num1 = request.args.get("num1")
+    num2 = request.args.get("num2")
+    if num1.isdigit() and num2.isdigit():
+        return jsonify({"result": int(num1) + int(num2)})
+    else:
+        return jsonify({"message": "Both numbers should be integers."}), 400
 
 if __name__ == "__main__":
     app.run(debug=True)
